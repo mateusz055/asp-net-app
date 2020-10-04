71176297620b79b47063eaa7289d2d349fe01f27
mateusz055 <mateusziskra591@gmail.com>
2020-10-04 12:56:31 +0200

    alphalogin

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(AccountViewModel avm)
+        {
+            if(avm.account.Username.Equals("admin")&& avm.account.Password.Equals("123"))
+            {
+                Session["username"] = avm.account.Username;
+                return View("Welcome");
+            }
+            else
+            {
+                ViewBag.Error = "Account's Invalid";
+                return View("Index");
+            }
+            
+        }
+    }
+}
---
+++ WebApplication1/ViewModels/AccountViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewModels
+{
+
+
+    public class AccountViewModel
+    {
+        public Account account
+        {
+            get;
+            set;
+        }
+
+    }
+
+}
